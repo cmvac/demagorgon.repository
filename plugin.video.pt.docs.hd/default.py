f697f27be01dab09830c3811b1f634b94d2edf0a
cmvac <cmvac@yahoo.com>
2017-11-16 15:19:52 +0100

    update

--- plugin.video.pt.docs.hd/default.py
+++ plugin.video.pt.docs.hd/default.py
@@ -75,7 +75,7 @@ def addon_log(string):
 def makeRequest(url, headers=None):
         try:
             if headers is None:
-                headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 507.40'}
+                headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 537.40'}
                 
             if '|' in url:
                 url,header_in_page=url.split('|')
