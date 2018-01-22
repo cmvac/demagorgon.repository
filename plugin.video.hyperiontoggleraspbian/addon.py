577bb8a379fce49ffcafb9342ff00b64b974c801
cmvac <cmvac@yahoo.com>
2018-01-22 23:14:53 +0100

    up... again.

--- plugin.video.hyperiontoggleraspbian/addon.py
+++ plugin.video.hyperiontoggleraspbian/addon.py
@@ -8,9 +8,9 @@ addonname   = addon.getAddonInfo('name')
      # kill hyperiond and rename config files
 cmd = "sudo systemctl stop hyperion.service 2>/dev/null; sudo /etc/init.d/hyperion stop 2>/dev/null ; sudo /sbin/initctl stop hyperion 2>/dev/null"
 os.system(cmd) 
-os.rename("/storage/.config/hyperion.config.json", "/storage/.config/hyperion.config2.json")
-os.rename("/storage/.config/hyperion.config1.json", "/storage/.config/hyperion.config.json")
-os.rename("/storage/.config/hyperion.config2.json", "/storage/.config/hyperion.config1.json")
+os.rename("/etc/hyperion/hyperion.config.json", "/etc/hyperion/hyperion.config2.json")
+os.rename("/etc/hyperion/hyperion.config1.json", "/etc/hyperion/hyperion.config.json")
+os.rename("/etc/hyperion/hyperion.config2.json", "/etc/hyperion/hyperion.config1.json")
 cmd = "sudo systemctl start hyperion.service 2>/dev/null ; sudo /etc/init.d/hyperion start 2>/dev/null ; sudo /sbin/initctl start hyperion 2>/dev/null"
 os.system(cmd)
 xbmcgui.Dialog().ok(addonname, "Config files swapped. You an swap between internal and external sources now")
