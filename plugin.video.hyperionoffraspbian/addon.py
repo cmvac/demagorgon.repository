a3f69864e125b55307399cb1281d327e21d20a21
cmvac <cmvac@yahoo.com>
2018-01-22 22:59:57 +0100

    up

--- plugin.video.hyperionoffraspbian/addon.py
+++ plugin.video.hyperionoffraspbian/addon.py
@@ -6,6 +6,6 @@ addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
 
      # kill hyperiond and rename config files
-cmd = "killall hyperiond"
+cmd = "sudo systemctl stop hyperion.service 2>/dev/null; sudo /etc/init.d/hyperion stop 2>/dev/null ; sudo /sbin/initctl stop hyperion 2>/dev/null"
 os.system(cmd) 
 xbmcgui.Dialog().ok(addonname, "Ambilight is now turned off.")
